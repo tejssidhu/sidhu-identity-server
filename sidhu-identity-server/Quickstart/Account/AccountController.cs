1d952d8ced5f27329ff1d52abdd22ba056761e04
Tej Sidhu <tejssidhu@gmail.com>
2018-01-27 14:26:05 +0000

    Add persistent store of data

--- sidhu-identity-server/Quickstart/Account/AccountController.cs
+++ sidhu-identity-server/Quickstart/Account/AccountController.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Authentication;
 using IdentityServer4.Events;
 using IdentityServer4.Extensions;
 using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace IdentityServer4.Quickstart.UI
 {
@@ -34,23 +35,23 @@ namespace IdentityServer4.Quickstart.UI
         private readonly IClientStore _clientStore;
         private readonly IAuthenticationSchemeProvider _schemeProvider;
         private readonly IEventService _events;
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly AccountService _account;
 
         public AccountController(
             IIdentityServerInteractionService interaction,
             IClientStore clientStore,
-            IAuthenticationSchemeProvider schemeProvider,
-            IEventService events,
-            TestUserStore users = null)
+			IHttpContextAccessor httpContextAccessor,
+			IAuthenticationSchemeProvider schemeProvider,
+			IEventService events,
+			UserManager<IdentityUser> userManager)
         {
-            // if the TestUserStore is not in DI, then we'll just use the global users collection
-            // this is where you would plug in your own custom identity management library (e.g. ASP.NET Identity)
-            _users = users ?? new TestUserStore(TestUsers.Users);
-
-            _interaction = interaction;
-            _clientStore = clientStore;
-            _schemeProvider = schemeProvider;
+			_schemeProvider = schemeProvider;
+			_userManager = userManager;
+			_interaction = interaction;
             _events = events;
-        }
+			_account = new AccountService(interaction, httpContextAccessor, schemeProvider, clientStore);
+		}
 
         /// <summary>
         /// Show login page
@@ -70,157 +71,196 @@ namespace IdentityServer4.Quickstart.UI
             return View(vm);
         }
 
-        /// <summary>
-        /// Handle postback from username/password login
-        /// </summary>
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginInputModel model, string button)
-        {
-            if (button != "login")
-            {
-                // the user clicked the "cancel" button
-                var context = await _interaction.GetAuthorizationContextAsync(model.ReturnUrl);
-                if (context != null)
-                {
-                    // if the user cancels, send a result back into IdentityServer as if they 
-                    // denied the consent (even if this client does not require consent).
-                    // this will send back an access denied OIDC error response to the client.
-                    await _interaction.GrantConsentAsync(context, ConsentResponse.Denied);
-                    
-                    // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
-                    return Redirect(model.ReturnUrl);
-                }
-                else
-                {
-                    // since we don't have a valid context, then we just go back to the home page
-                    return Redirect("~/");
-                }
-            }
+		/// <summary>
+		/// Handle postback from username/password login
+		/// </summary>
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Login(LoginInputModel model, string button)
+		{
+			if (button != "login")
+			{
+				var context = await _interaction.GetAuthorizationContextAsync(model.ReturnUrl);
+				if (context != null)
+				{
+					await _interaction.GrantConsentAsync(context, ConsentResponse.Denied);
+					return Redirect(model.ReturnUrl);
+				}
+				else
+				{
+					return Redirect("~/");
+				}
+			}
 
-            if (ModelState.IsValid)
-            {
-                // validate username/password against in-memory store
-                if (_users.ValidateCredentials(model.Username, model.Password))
-                {
-                    var user = _users.FindByUsername(model.Username);
-                    await _events.RaiseAsync(new UserLoginSuccessEvent(user.Username, user.SubjectId, user.Username));
+			if (ModelState.IsValid)
+			{
+				var user = await _userManager.FindByNameAsync(model.Username);
 
-                    // only set explicit expiration here if user chooses "remember me". 
-                    // otherwise we rely upon expiration configured in cookie middleware.
-                    AuthenticationProperties props = null;
-                    if (AccountOptions.AllowRememberLogin && model.RememberLogin)
-                    {
-                        props = new AuthenticationProperties
-                        {
-                            IsPersistent = true,
-                            ExpiresUtc = DateTimeOffset.UtcNow.Add(AccountOptions.RememberMeLoginDuration)
-                        };
-                    };
+				if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+				{
+					await _events.RaiseAsync(
+						new UserLoginSuccessEvent(user.UserName, user.Id, user.UserName));
 
-                    // issue authentication cookie with subject ID and username
-                    await HttpContext.SignInAsync(user.SubjectId, user.Username, props);
+					AuthenticationProperties props = null;
+					if (AccountOptions.AllowRememberLogin && model.RememberLogin)
+					{
+						props = new AuthenticationProperties
+						{
+							IsPersistent = true,
+							ExpiresUtc = DateTimeOffset.UtcNow.Add(AccountOptions.RememberMeLoginDuration)
+						};
+					};
 
-                    // make sure the returnUrl is still valid, and if so redirect back to authorize endpoint or a local page
-                    // the IsLocalUrl check is only necessary if you want to support additional local pages, otherwise IsValidReturnUrl is more strict
-                    if (_interaction.IsValidReturnUrl(model.ReturnUrl) || Url.IsLocalUrl(model.ReturnUrl))
-                    {
-                        return Redirect(model.ReturnUrl);
-                    }
+					await HttpContext.SignInAsync(user.Id, user.UserName, props);
 
-                    return Redirect("~/");
-                }
+					if (_interaction.IsValidReturnUrl(model.ReturnUrl)
+							|| Url.IsLocalUrl(model.ReturnUrl))
+					{
+						return Redirect(model.ReturnUrl);
+					}
 
-                await _events.RaiseAsync(new UserLoginFailureEvent(model.Username, "invalid credentials"));
+					return Redirect("~/");
+				}
 
-                ModelState.AddModelError("", AccountOptions.InvalidCredentialsErrorMessage);
-            }
+				await _events.RaiseAsync(new UserLoginFailureEvent(model.Username, "invalid credentials"));
+				ModelState.AddModelError("", AccountOptions.InvalidCredentialsErrorMessage);
+			}
 
-            // something went wrong, show form with error
-            var vm = await BuildLoginViewModelAsync(model);
-            return View(vm);
-        }
+			var vm = await _account.BuildLoginViewModelAsync(model);
+			return View(vm);
+		}
 
-        /// <summary>
-        /// initiate roundtrip to external authentication provider
-        /// </summary>
-        [HttpGet]
+		/// <summary>
+		/// initiate roundtrip to external authentication provider
+		/// </summary>
+		[HttpGet]
         public async Task<IActionResult> ExternalLogin(string provider, string returnUrl)
         {
-            if (AccountOptions.WindowsAuthenticationSchemeName == provider)
-            {
-                // windows authentication needs special handling
-                return await ProcessWindowsLoginAsync(returnUrl);
-            }
-            else
-            {
-                // start challenge and roundtrip the return URL and 
-                var props = new AuthenticationProperties()
-                {
-                    RedirectUri = Url.Action("ExternalLoginCallback"),
-                    Items =
-                    {
-                        { "returnUrl", returnUrl },
-                        { "scheme", provider },
-                    }
-                };
-                return Challenge(props, provider);
-            }
-        }
+			var props = new AuthenticationProperties()
+			{
+				RedirectUri = Url.Action("ExternalLoginCallback"),
+				Items =
+				{
+					{ "returnUrl", returnUrl }
+				}
+			};
 
-        /// <summary>
-        /// Post processing of external authentication
-        /// </summary>
-        [HttpGet]
-        public async Task<IActionResult> ExternalLoginCallback()
-        {
-            // read external identity from the temporary cookie
-            var result = await HttpContext.AuthenticateAsync(IdentityServer4.IdentityServerConstants.ExternalCookieAuthenticationScheme);
-            if (result?.Succeeded != true)
-            {
-                throw new Exception("External authentication error");
-            }
+			// windows authentication needs special handling
+			// since they don't support the redirect uri, 
+			// so this URL is re-triggered when we call challenge
+			if (AccountOptions.WindowsAuthenticationSchemeName == provider)
+			{
+				// see if windows auth has already been requested and succeeded
+				var result = await HttpContext.AuthenticateAsync(AccountOptions.WindowsAuthenticationSchemeName);
+				if (result?.Principal is WindowsPrincipal wp)
+				{
+					props.Items.Add("scheme", AccountOptions.WindowsAuthenticationSchemeName);
 
-            // lookup our user and external provider info
-            var (user, provider, providerUserId, claims) = FindUserFromExternalProvider(result);
-            if (user == null)
-            {
-                // this might be where you might initiate a custom workflow for user registration
-                // in this sample we don't show how that would be done, as our sample implementation
-                // simply auto-provisions new external user
-                user = AutoProvisionUser(provider, providerUserId, claims);
-            }
+					var id = new ClaimsIdentity(provider);
+					id.AddClaim(new Claim(JwtClaimTypes.Subject, wp.Identity.Name));
+					id.AddClaim(new Claim(JwtClaimTypes.Name, wp.Identity.Name));
 
-            // this allows us to collect any additonal claims or properties
-            // for the specific prtotocols used and store them in the local auth cookie.
-            // this is typically used to store data needed for signout from those protocols.
-            var additionalLocalClaims = new List<Claim>();
-            var localSignInProps = new AuthenticationProperties();
-            ProcessLoginCallbackForOidc(result, additionalLocalClaims, localSignInProps);
-            ProcessLoginCallbackForWsFed(result, additionalLocalClaims, localSignInProps);
-            ProcessLoginCallbackForSaml2p(result, additionalLocalClaims, localSignInProps);
+					// add the groups as claims -- be careful if the number of groups is too large
+					if (AccountOptions.IncludeWindowsGroups)
+					{
+						var wi = wp.Identity as WindowsIdentity;
+						var groups = wi.Groups.Translate(typeof(NTAccount));
+						var roles = groups.Select(x => new Claim(JwtClaimTypes.Role, x.Value));
+						id.AddClaims(roles);
+					}
 
-            // issue authentication cookie for user
-            await _events.RaiseAsync(new UserLoginSuccessEvent(provider, providerUserId, user.SubjectId, user.Username));
-            await HttpContext.SignInAsync(user.SubjectId, user.Username, provider, localSignInProps, additionalLocalClaims.ToArray());
+					await HttpContext.SignInAsync(
+						IdentityConstants.ExternalScheme,
+						new ClaimsPrincipal(id),
+						props);
+					return Redirect(props.RedirectUri);
+				}
+				else
+				{
+					// challenge/trigger windows auth
+					return Challenge(AccountOptions.WindowsAuthenticationSchemeName);
+				}
+			}
+			else
+			{
+				// start challenge and roundtrip the return URL
+				props.Items.Add("scheme", provider);
+				return Challenge(props, provider);
+			}
+		}
 
-            // delete temporary cookie used during external authentication
-            await HttpContext.SignOutAsync(IdentityServer4.IdentityServerConstants.ExternalCookieAuthenticationScheme);
+		/// <summary>
+		/// Post processing of external authentication
+		/// </summary>
+		[HttpGet]
+		public async Task<IActionResult> ExternalLoginCallback()
+		{
+			var result = await HttpContext.AuthenticateAsync(IdentityConstants.ExternalScheme);
+			if (result?.Succeeded != true)
+			{
+				throw new Exception("External authentication error");
+			}
 
-            // validate return URL and redirect back to authorization endpoint or a local page
-            var returnUrl = result.Properties.Items["returnUrl"];
-            if (_interaction.IsValidReturnUrl(returnUrl) || Url.IsLocalUrl(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
+			var externalUser = result.Principal;
+			var claims = externalUser.Claims.ToList();
 
-            return Redirect("~/");
-        }
+			var userIdClaim = claims.FirstOrDefault(x => x.Type == JwtClaimTypes.Subject);
+			if (userIdClaim == null)
+			{
+				userIdClaim = claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+			}
+			if (userIdClaim == null)
+			{
+				throw new Exception("Unknown userid");
+			}
 
-        /// <summary>
-        /// Show logout page
-        /// </summary>
-        [HttpGet]
+			claims.Remove(userIdClaim);
+			var provider = result.Properties.Items["scheme"];
+			var userId = userIdClaim.Value;
+
+			var user = await _userManager.FindByLoginAsync(provider, userId);
+			if (user == null)
+			{
+				user = new IdentityUser { UserName = Guid.NewGuid().ToString() };
+				await _userManager.CreateAsync(user);
+				await _userManager.AddLoginAsync(user, new UserLoginInfo(provider, userId, provider));
+			}
+
+			var additionalClaims = new List<Claim>();
+
+			var sid = claims.FirstOrDefault(x => x.Type == JwtClaimTypes.SessionId);
+			if (sid != null)
+			{
+				additionalClaims.Add(new Claim(JwtClaimTypes.SessionId, sid.Value));
+			}
+
+			AuthenticationProperties props = null;
+			var id_token = result.Properties.GetTokenValue("id_token");
+			if (id_token != null)
+			{
+				props = new AuthenticationProperties();
+				props.StoreTokens(new[] { new AuthenticationToken { Name = "id_token", Value = id_token } });
+			}
+
+			await _events.RaiseAsync(new UserLoginSuccessEvent(provider, userId, user.Id, user.UserName));
+			await HttpContext.SignInAsync(
+				user.Id, user.UserName, provider, props, additionalClaims.ToArray());
+
+			await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+			var returnUrl = result.Properties.Items["returnUrl"];
+			if (_interaction.IsValidReturnUrl(returnUrl) || Url.IsLocalUrl(returnUrl))
+			{
+				return Redirect(returnUrl);
+			}
+
+			return Redirect("~/");
+		}
+
+		/// <summary>
+		/// Show logout page
+		/// </summary>
+		[HttpGet]
         public async Task<IActionResult> Logout(string logoutId)
         {
             // build a model so the logout page knows what to display
--- sidhu-identity-server/Startup.cs
+++ sidhu-identity-server/Startup.cs
@@ -2,21 +2,53 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.EntityFramework.DbContexts;
+using System.Linq;
 
 namespace sidhu_identity_server
 {
     public class Startup
     {
+        public IHostingEnvironment HostingEnvironment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env, IConfiguration config)
+        {
+            HostingEnvironment = env;
+            Configuration = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-	        services.AddIdentityServer()
-		        .AddInMemoryClients(Clients.Get())
-		        .AddInMemoryIdentityResources(Resources.GetIdentityResources())
-		        .AddInMemoryApiResources(Resources.GetApiResources())
-		        .AddTestUsers(Users.Get())
-		        .AddDeveloperSigningCredential();
+            var connectionString = Configuration.GetConnectionString("IdentityServerStore");
+			var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+			services.AddDbContext<ApplicationDbContext>(builder =>
+				builder.UseSqlServer(connectionString, sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)));
+
+			services.AddIdentity<IdentityUser, IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			services.AddIdentityServer()
+				.AddOperationalStore(options => 
+					options.ConfigureDbContext = builder => 
+						builder.UseSqlServer(connectionString, sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)))
+				.AddConfigurationStore(options =>
+					options.ConfigureDbContext = builder =>
+						builder.UseSqlServer(connectionString, sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)))
+				.AddAspNetIdentity<IdentityUser>()
+				//.AddInMemoryClients(Clients.Get())
+				//.AddInMemoryIdentityResources(Resources.GetIdentityResources())
+				//.AddInMemoryApiResources(Resources.GetApiResources())
+				//.AddTestUsers(Users.Get())
+				.AddDeveloperSigningCredential();
 
             services.AddMvc();
         }
@@ -29,7 +61,9 @@ namespace sidhu_identity_server
                 app.UseDeveloperExceptionPage();
             }
 
-	        app.UseIdentityServer();
+			InitializeDbTestData(app);
+
+			app.UseIdentityServer();
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
@@ -39,5 +73,59 @@ namespace sidhu_identity_server
                 await context.Response.WriteAsync("Hello World!");
             });
         }
-    }
+
+		private static void InitializeDbTestData(IApplicationBuilder app)
+		{
+			using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+			{
+				scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+				scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
+				scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
+
+				var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+				if (!context.Clients.Any())
+				{
+					foreach (var client in Clients.Get())
+					{
+						context.Clients.Add(client.ToEntity());
+					}
+					context.SaveChanges();
+				}
+
+				if (!context.IdentityResources.Any())
+				{
+					foreach (var resource in Resources.GetIdentityResources())
+					{
+						context.IdentityResources.Add(resource.ToEntity());
+					}
+					context.SaveChanges();
+				}
+
+				if (!context.ApiResources.Any())
+				{
+					foreach (var resource in Resources.GetApiResources())
+					{
+						context.ApiResources.Add(resource.ToEntity());
+					}
+					context.SaveChanges();
+				}
+
+				var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+				if (!userManager.Users.Any())
+				{
+					foreach (var testUser in Users.Get())
+					{
+						var identityUser = new IdentityUser(testUser.Username)
+						{
+							Id = testUser.SubjectId
+						};
+
+						userManager.CreateAsync(identityUser, "Password123!").Wait();
+						userManager.AddClaimsAsync(identityUser, testUser.Claims.ToList()).Wait();
+					}
+				}
+			}
+		}
+	}
 }
